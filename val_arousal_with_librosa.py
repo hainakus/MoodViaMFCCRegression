99920a1f0114fe1a51892db203b7e4b5350c6016
PrimozGodec <p.godec9@gmail.com>
2014-06-27 14:25:48 +0200

    graph range fix

--- val_arousal_with_librosa.py
+++ val_arousal_with_librosa.py
@@ -120,6 +120,7 @@ def show_for_id(song_id, valence, arousal, ids, valence_dict, arousal_dict):
     plt.plot(valence_dict[song_id], arousal_dict[song_id], 'o', color='green', markersize=1)
     plt.plot(valence[idx], arousal[idx], 'o', color='blue')
     plt.plot(sum(valence_dict[song_id])/float(len(valence_dict[song_id])), sum(arousal_dict[song_id])/float(len(arousal_dict[song_id])), 'o', color='red')
+    plt.axis([-1, 1, -1, 1])
     plt.savefig('results/' + str(song_id) + '.png')
 
 
