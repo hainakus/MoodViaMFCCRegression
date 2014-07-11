4595513346abb3af524bb9ea7638b740f37153eb
Primoz Godec <p.godec9@gmail.com>
2014-07-11 14:57:13 +0200

    erik chroma export algorithm

---
+++ eric_dataset/chroma2file.m
@@ -0,0 +1,28 @@
+% function get mfcc data from .mat files from Erik Schmidt
+% these data exports in text file
+
+names = []
+data = []
+
+files = dir('*.mat');
+
+for i = 1:length(files)
+    name = files(i).name
+    load(name);
+    
+    % get mena of mfccs
+    % return 20x1 matrix
+    mfcc = mean(features.chroma, 2);    
+    
+    data = [data; mfcc'];
+    size(data)
+end
+size(data)
+fid = fopen('chroma.csv','w');
+for i = 1:length(files)
+    name = files(i).name;
+    fprintf(fid,'%s,',name,data(i,:));
+    fprintf(fid,'%f,',name,data(i,:));
+   	fprintf(fid, '\n');
+end
+fclose(fid)
